923744e871106503870aac04590284e6dbe65c3d
Savage <savageboy74@gmail.com>
2016-02-09 20:52:19 -0500

    ...

--- config/container.config.php
+++ config/container.config.php
@@ -7,8 +7,8 @@ return [
       'mysql' => [
         'driver' => 'mysql',
         'host' => '127.0.0.1',
-        'username' => 'dev',
-        'password' => 'dev',
+        'username' => 'root',
+        'password' => '',
         'database' => 'demoapp',
         'charset' => 'utf8',
         'collation' => 'utf8_unicode_ci',
