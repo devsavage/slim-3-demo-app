b85860abe28ef0f3f6bf8730599e41151d16546e
Savage <savageboy74@gmail.com>
2016-02-10 20:04:20 -0500

    Update sql/demoapp.sql to support is_head_admin perm

--- sql/demoapp.sql
+++ sql/demoapp.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               5.6.25 - MySQL Community Server (GPL)
+-- Server version:               10.1.9-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win32
--- HeidiSQL Version:             9.3.0.5026
+-- HeidiSQL Version:             9.3.0.4984
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS `permissions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `is_admin` tinyint(4) NOT NULL DEFAULT '0',
+  `is_head_admin` tinyint(4) DEFAULT '0',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
